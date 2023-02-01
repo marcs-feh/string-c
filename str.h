9faccf98f174338f5eeb01dc68495d8c2eea97b3
marcs-feh <marcosfehlauer@protonmail.com>
2023-02-01 19:48:48 -0300

    interface

---
+++ str.h
@@ -0,0 +1,30 @@
+#ifndef INCLUDE_STR_H_
+#define INCLUDE_STR_H_
+
+#include "types.h"
+
+// Sequence of bytes
+typedef struct {
+	usize size;
+	byte* buf;
+} Byte_Seq;
+
+typedef struct {
+	usize size;    // String lenght, not including NUL terminator.
+	Byte_Seq data; // String storage, UTF-8 bytes.
+} String;
+
+Byte_Seq byte_seq_new(usize n);
+void     byte_seq_del(Byte_Seq* seq);
+void     byte_seq_resize(Byte_Seq* seq, usize n);
+
+String string_new(usize size_hint);
+String string_from_cstr(const char* cs);
+void   string_del(String* s);
+void   string_copy(String* dest, String* src);
+void   string_move(String* dest, String* src);
+void   string_append_byte(String* s, byte b);
+// void string_append_rune(String* s, byte b);
+
+
+#endif /* include guard */
---
+++ types.h
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_TYPES_H_
+#define INCLUDE_TYPES_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdalign.h>
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float    f32;
+typedef double   f64;
+
+typedef size_t        usize;
+typedef uintptr_t     uintptr;
+typedef unsigned char byte;
+typedef unsigned int  uint;
+typedef uint32_t      rune;
+
+#endif /* include guard */
