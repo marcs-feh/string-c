b3d7bd46e921e20ec2564ff79d2e2c5512eeb6ef
marcs-feh <marcosfehlauer@protonmail.com>
2023-02-02 00:55:19 -0300

    use function to deduce octet lenght

--- main.c
+++ main.c
@@ -7,22 +7,18 @@
 int main(int argc, const char** argv){
 	rune point = '$';
 	Octet oc = encode_rune(point);
-	printf("$\t ");
 	print_octet(oc);
 
 	point = 0x00a3;
 	oc = encode_rune(point);
-	printf("£\t ");
 	print_octet(oc);
 
 	point = 0xd55c;
 	oc = encode_rune(point);
-	printf("한\t ");
 	print_octet(oc);
 
 	point = 0x10348;
 	oc = encode_rune(point);
-	printf("𐍈\t");
 	print_octet(oc);
 	return 0;
 }
--- utf8.c
+++ utf8.c
@@ -1,7 +1,6 @@
 #include "types.h"
 
 typedef struct {
-	u32  size;
 	byte data[4];
 } Octet;
 
@@ -14,19 +13,16 @@ Octet encode_rune(rune r){
 	Octet oc = {0};
 	if(r <= utf8_range_1byte){
 		// 0xxxxxxx
-		oc.size = 1;
 		oc.data[0] = r;
 	}
 	else if(r <= utf8_range_2byte){
 		// 110xxxxx 10xxxxxx
-		oc.size = 2;
 		oc.data[1] = (r & 0x3f) | 0x80;
 		r = r >> 6;
 		oc.data[0] = r | 0xc0; // TODO eliminate &?
 	}
 	else if(r <= utf8_range_3byte){
-		// 0x1110xxxx 10xxxxxx 10xxxxxx
-		oc.size = 3;
+		// 1110xxxx 10xxxxxx 10xxxxxx
 		oc.data[2] = (r & 0x3f) | 0x80;
 		r = r >> 6;
 		oc.data[1] = (r & 0x3f) | 0x80;
@@ -34,8 +30,7 @@ Octet encode_rune(rune r){
 		oc.data[0] = r | 0xe0;
 	}
 	else if(r <= utf8_range_4byte){
-		// 0x11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-		oc.size = 4;
+		// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
 		oc.data[3] = (r & 0x3f) | 0x80;
 		r = r >> 6;
 		oc.data[2] = (r & 0x3f) | 0x80;
@@ -48,6 +43,20 @@ Octet encode_rune(rune r){
 	return oc;
 }
 
+// Get length of an octet based on its first byte
+uint octet_len(byte b){
+	if (!(b & 0x80)){ return 1; }
+	else if((b >> 5) == 0x06) { return 2; }
+	else if((b >> 4) == 0x0e) { return 3; }
+	else if((b >> 3) == 0x1e) { return 4; }
+	else { return 0; }
+}
+
+// return 0
+// 	+ (((b >> 5) == 0x06) * 2)
+// 	+ (((b >> 4) == 0x0e) * 3)
+// 	+ (((b >> 3) == 0x1e) * 4);
+
 #include <stdio.h>
 
 void byte_as_bin_str(char buf[8], byte b){
@@ -58,22 +67,36 @@ void byte_as_bin_str(char buf[8], byte b){
 
 void print_octet(Octet oc){
 	char buf[9];
+	byte databuf[5];
+	uint len = octet_len(oc.data[0]);
+	for(uint i = 0; i < 4; i += 1){
+		databuf[i] = oc.data[i];
+	}
 	buf[8] = '\0';
-	if(oc.size >= 1){
+
+	switch(len){
+		case 1: databuf[1] = '\0'; break;
+		case 2: databuf[2] = '\0'; break;
+		case 3: databuf[3] = '\0'; break;
+		case 4: databuf[4] = '\0'; break;
+	}
+	printf("%s\t| ", databuf);
+
+	if(len >= 1){
 		byte_as_bin_str(buf, oc.data[0]);
 		printf("%s ", buf);
 	}
-	if(oc.size >= 2){
+	if(len >= 2){
 		byte_as_bin_str(buf, oc.data[1]);
 		printf("%s ", buf);
 	}
-	if(oc.size >= 3){
+	if(len >= 3){
 		byte_as_bin_str(buf, oc.data[2]);
 		printf("%s ", buf);
 	}
-	if(oc.size >= 4){
+	if(len >= 4){
 		byte_as_bin_str(buf, oc.data[3]);
-		printf("%s ", buf);
+		printf("%s\n", buf);
 	}
 	printf("\n");
 }
